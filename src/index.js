0cb4e80ab83d265e1a909ded882a04aca8b5a6e3
anastasiiahmel <126502776+anastasiiahmel@users.noreply.github.com>
2023-10-29 17:33:45 +0200

    11

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-08-phonebook">
       <App />
     </BrowserRouter>
   </React.StrictMode>
