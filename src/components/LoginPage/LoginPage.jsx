0e75f9fe59e1af0dd8aa13d834792b6b35ff171e
Nastya Khmelenko <126502776+anastasiiahmel@users.noreply.github.com>
2024-09-17 15:18:23 +0100

    a bug was fixed when logging in, there was a problem with the fact that if the password was incorrect, the user did not know about it

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
 import { message } from 'antd';
-
 import { useDispatch, useSelector } from 'react-redux';
-import {selectLogIn } from 'redux/auth/selectors';
+import { selectLogIn } from 'redux/auth/selectors';
 import { loginUser } from 'redux/auth/operations';
-import { BtnAuth, FormLogin, GeneralInfo, InputInfo, InputPassword, TitlePage } from './LoginStyles.styled';
+import {
+  BtnAuth,
+  FormLogin,
+  GeneralInfo,
+  InputInfo,
+  InputPassword,
+  TitlePage,
+} from './LoginStyles.styled';
 
 const LoginPage = () => {
   const dispatch = useDispatch();
@@ -16,60 +21,59 @@ const LoginPage = () => {
     email: '',
     password: '',
   });
-  
+
   useEffect(() => {
-    isLoggedIn && navigate('/contacts');
+    if (isLoggedIn) {
+      navigate('/contacts');
+    }
   }, [isLoggedIn, navigate]);
 
-
-
-  const handleChange = (e) => {
+  const handleChange = e => {
     const { name, value } = e.target;
     setFormData(prevState => ({
       ...prevState,
       [name]: value,
     }));
   };
-  
-  const handleSubmit = async (e) => {
+
+  const handleSubmit = async e => {
     e.preventDefault();
+
     const dataUser = {
       email: formData.email,
       password: formData.password,
     };
 
-    dispatch(loginUser(dataUser))
-    .then(() => {  
+    try {
+      await dispatch(loginUser(dataUser)).unwrap();
       message.success('Login success!');
-    }).catch((e) => {
+    } catch (error) {
       message.error('Login error, Email or Password wrong!');
-  });
+    }
   };
 
   return (
     <GeneralInfo>
-      <TitlePage >Authentication</TitlePage>
-        <FormLogin onSubmit={handleSubmit}>
-          <InputInfo
-            placeholder="Email"
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-          />
-          <InputPassword
-            placeholder="Password"
-            id="password"
-            name="password"
-            type="password"
-            value={formData.password}
-            onChange={handleChange}
-            variant="outlined"
-          />
-          <BtnAuth type="primary" htmlType="submit">
-            Sign in
-          </BtnAuth>
-        </FormLogin>
+      <TitlePage>Authentication</TitlePage>
+      <FormLogin onSubmit={handleSubmit}>
+        <InputInfo
+          placeholder="Email"
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <InputPassword
+          placeholder="Password"
+          name="password"
+          type="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <BtnAuth type="primary" htmlType="submit">
+          Sign in
+        </BtnAuth>
+      </FormLogin>
     </GeneralInfo>
   );
 };
